93fa693d27cc96325a41736448d7c722fed5431d
Annie Truex <artruex@gmail.com>
2019-05-20 00:55:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -3,12 +3,11 @@ numbers = [1,2,3]
 def square_array(array)
   # your code here
   new_numbers = []
-  array.each { |element| square_array << element ** 2 }
-  square_array(new_numbers)
+  
 end
 
   new_numbers = []
-end 	  numbers.each do |num|
+  numbers.each do |num|
     new_numbers << (num ** 2)
   end
   return new_numbers
