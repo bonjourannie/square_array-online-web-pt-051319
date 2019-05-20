e76963faef81baaef3406408b9030d1a0c3ebc69
Annie Truex <artruex@gmail.com>
2019-05-20 00:53:09 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -2,7 +2,7 @@ numbers = [1,2,3]
 
 def square_array(array)
   # your code here
-  squared []
+  new_numbers = []
   array.each { |element| squared << element ** 2 }
-  square_array
+  square_array(new_numbers)
 end
